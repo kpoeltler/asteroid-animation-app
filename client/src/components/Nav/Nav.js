05be51a9e25a5b1c7b5dc4e26a8f1dc46a1fa9ec
Katie Poeltler <kpoeltler@gmail.com>
2018-04-02 23:31:30 -0500

    servers

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -2,8 +2,7 @@
 import React from 'react';
 import  './Nav.css';
 
-const Nav = () => 
-  
+const Nav = () => (
   <nav className="navbar navbar-inverse navbar-top">
     <div id="animate-area" style={
       { height: 90, clear: "both", 
@@ -13,20 +12,22 @@ const Nav = () =>
     backgroundRepeat: "repeat-x",
     animation: "animatedBackground 15s linear infinite",
     borderColor: "#040009"  }}
-    
-    className="container-fluid">;
-      <div>
-        
+  
+    className="container-fluid">
+    <div className="navbar-header">
+      <div>  
         <button type="button" className="collapsed navbar-toggle">
           <span className="sr-only">Toggle navigation</span>
           <span className="icon-bar" /> <span className="icon-bar" />
           <span className="icon-bar" />
         </button>
+        <a href="/" className="navbar-brand">
         <h1 style={{ color:'white', fontWeight: '900' }} >Through The Telescope</h1>
-        
+        </a>
       </div>
        </div>   
+       </div>
   </nav> 
-
+);
 
 export default Nav;
--- models/User.js
+++ models/User.js
@@ -1,31 +1,31 @@
-var mongoose = require("mongoose");
+// var mongoose = require("mongoose");
 
 // Save a reference to the Schema constructor
-var Schema = mongoose.Schema;
+// var Schema = mongoose.Schema;
 
 // Using the Schema constructor, create a new UserSchema object
 // This is similar to a Sequelize model
-var UserSchema = new Schema({
+// var UserSchema = new Schema({
   // `name` must be unique and of type String
-  name: {
-    type: String,
-    unique: true
-  },
+  // name: {
+  //   type: String,
+  //   unique: true
+  // },
   // `notes` is an array that stores ObjectIds
   // The ref property links these ObjectIds to the Note model
   // This allows us to populate the User with any associated Notes
-  notes: [
-    {
+  // notes: [
+  //   {
       // Store ObjectIds in the array
-      type: Schema.Types.ObjectId,
+      // type: Schema.Types.ObjectId,
       // The ObjectIds will refer to the ids in the Note model
-      ref: "Note"
-    }
-  ]
-});
+//       ref: "Note"
+//     }
+//   ]
+// });
 
 // This creates our model from the above schema, using mongoose's model method
-var User = mongoose.model("User", UserSchema);
+// var User = mongoose.model("User", UserSchema);
 
 // Export the User model
-module.exports = User;
+// module.exports = User;
