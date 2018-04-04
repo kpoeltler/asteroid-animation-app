357471a9b40883b743edab0c1e1a953389dfe03e
Katie Poeltler <kpoeltler@gmail.com>
2018-04-04 18:23:04 -0500

    drop down menu for solar works and I took out the console.logs for Entry.js

--- client/src/pages/Entry/Entry.js
+++ client/src/pages/Entry/Entry.js
@@ -5,188 +5,256 @@ import { Col, Row, Container } from "../../components/Grid";
 import { Input, TextArea, FormBtn } from "../../components/Form";
 import Nav from "../../components/Nav";
 
-
 class Entry extends Component {
-  
-    state = {
-      bc: false,
-      celestron: false,
-      meade: false,
-      coronado: false,
-      entries: [],
-      telescope: "",
-      filter: "",
-      asteroid: "",
-      orbit: "",
+  state = {
+    bc: false,
+    celestron: false,
+    meade: false,
+    coronado: false,
+    entries: [],
+    telescope: "",
+    filter: "",
+    asteroid: "",
+    orbit: "",
 
-      comment: ""
-    };
+    comment: ""
+  };
 
-    componentDidMount() {
-      this.loadEntries();
+  componentDidMount() {
+    this.loadEntries();
+  }
+
+  loadEntries = () => {
+    API.getEntries()
+      .then(res =>
+        this.setState({
+          entries: res.data,
+          filter: "",
+          telescope: "",
+          asteroid: "",
+          orbit: "",
+          comment: ""
+        })
+      )
+      .catch(err => console.log(err));
+  };
+
+  handleTelescopeChange = event => {
+    switch (event.target.value) {
+      case "celestron":
+        this.setState(
+          { celestron: true, telescope: event.target.value },
+          function() {}
+        );
+        break;
+
+      case "meade":
+        this.setState(
+          { meade: true, telescope: event.target.value },
+          function() {}
+        );
+        break;
+
+      case "bc":
+        this.setState(
+          { bc: true, telescope: event.target.value },
+          function() {}
+        );
+        break;
+
+      case "coronado":
+        this.setState(
+          { coronado: true, telescope: event.target.value },
+          function() {}
+        );
+        break;
     }
-
-    loadEntries = () => {
-      API.getEntries()
-        .then(res =>
-          this.setState({ entries: res.data, filter: "", telescope: "", asteroid: "", orbit: "", comment: "" })
-        )
-        .catch(err => console.log(err));
-    };
-  
-    handleTelescopeChange = event => {
-      console.log("update the state with new telescope", event.target.value);
-      switch(event.target.value){
-        case "celestron":
-        this.setState({celestron:true, telescope:event.target.value}, function(){
-          console.log("celestron update",this.state.celestron);
-        }) 
-        break;
-
-        case "meade":
-        this.setState({meade:true, telescope:event.target.value}, function(){
-          console.log("meade update",this.state.meade);
-        }) 
-        break;
-
-        case "bc":
-        this.setState({bc:true, telescope:event.target.value}, function(){
-          console.log("bc update",this.state.bc);
-        }) 
-        break;
-
-        case "coronado":
-        this.setState({coronado:true, telescope:event.target.value}, function(){
-          console.log("coronado update",this.state.bc);
-        }) 
-        break;
-      }
-    };
+  };
 
   handleInputChange = event => {
-    console.log("update the state with new stuff", event.target.value);
     const { name, value } = event.target;
-    this.setState({ [name]: value
-     });
+    this.setState({
+      [name]: value
+    });
   };
 
   handleFormSubmit = event => {
     event.preventDefault();
-    console.log("hi");
-    // if (this.state.comment) {
-      console.log("good comment");
-      API.saveEntry ({
-        telescope: this.state.telescope,
-        target: this.state.target,
-        orbit: this.state.orbit,
-        filter: this.state.filter,
-        comment: this.state.comment
+    API.saveEntry({
+      telescope: this.state.telescope,
+      target: this.state.target,
+      orbit: this.state.orbit,
+      filter: this.state.filter,
+      comment: this.state.comment
+    })
+      .then(res => {
+        this.loadEntries();
       })
-        .then(res => {
-          console.log(res);
-          this.loadEntries()
-        })
-        .catch(err => console.log(err));
-    // }
+      .catch(err => console.log(err));
   };
-  
+
   render() {
     return (
       <Container fluid>
         <Row>
           <Col size="sm-12">
-          <Nav />
+            <Nav />
             <Jumbotron>
               <form>
                 <div>
-                  <br/>
-                                <span> Telescope </span>
-                            <select
-                                value={this.state.telescope}
-                                onChange={this.handleTelescopeChange}
-                                name="telescope"
-                                placeholder="telescope"
-                            >
-                                <option value=""></option>
-                                <option value='bc' >24" Boller & Chivens</option>
-                                <option value='celestron'>14" Celestron</option>
-                                <option value='meade'>16" Meade</option>
-                                <option value='coronado'>Coronado P.S.T.</option>    
+                  <br />
+                  <span> Telescope </span>
+                  <select
+                    value={this.state.telescope}
+                    onChange={this.handleTelescopeChange}
+                    name="telescope"
+                    placeholder="telescope"
+                  >
+                    <option value="" />
+                    <option value="bc">24" Boller & Chivens</option>
+                    <option value="celestron">14" Celestron</option>
+                    <option value="meade">16" Meade</option>
+                    <option value="coronado">Coronado P.S.T.</option>
                   </select>
-                  </div>
-                  <br/>
+                </div>
+                <br />
 
                 <div>
-                  <br/>
-                                <span> Target </span>
-                            <select
-                                value={this.state.target}
-                                onChange={this.handleInputChange}
-                                name="target"
-                                placeholder="Choose target"
-                            >
-                                <option value=""></option>
-                                <option value="Asteroid" hidden = {this.state.celestron}>Asteroid</option>
-                                <option value="Variable Star">Variable Star</option>
-                                <option value="Globular Cluster" hidden = {this.state.celestron} >Globular Cluster</option>
-                                <option value="Binary Stars">Binary Stars</option>
-                                <option value="Planet">Planet</option>
-                                <option value="Moon">Moon</option>
-                                <option value="Galaxy">Galaxy</option>
-                                <option value="Sun" hidden={(this.state.celestron || this.state.meade) }> Sun</option>
+                  <br />
+                  <span> Target </span>
+                  <select
+                    value={this.state.target}
+                    onChange={this.handleInputChange}
+                    name="target"
+                    placeholder="Choose target"
+                  >
+                    <option value="" />
+                    <option
+                      value="Asteroid"
+                      hidden={
+                        this.state.celestron ||
+                        this.state.meade ||
+                        this.state.coronado
+                      }
+                    >
+                      Asteroid
+                    </option>
+                    <option value="Variable Star" hidden={this.state.coronado}>
+                      Variable Star
+                    </option>
+                    <option
+                      value="Globular Cluster"
+                      hidden={
+                        this.state.celestron ||
+                        this.state.meade ||
+                        this.state.coronado
+                      }
+                    >
+                      Globular Cluster
+                    </option>
+                    <option value="Binary Stars" hidden={this.state.coronado}>
+                      Binary Stars
+                    </option>
+                    <option value="Planet" hidden={this.state.coronado}>
+                      Planet
+                    </option>
+                    <option value="Moon" hidden={this.state.coronado}>
+                      Moon
+                    </option>
+                    <option
+                      value="Galaxy"
+                      hidden={
+                        this.state.meade || this.state.bc || this.state.coronado
+                      }
+                    >
+                      Galaxy
+                    </option>
+
+                    <option
+                      value="Sun"
+                      hidden={
+                        this.state.celestron ||
+                        this.state.meade ||
+                        this.state.bc
+                      }
+                    >
+                      {" "}
+                      Sun
+                    </option>
                   </select>
-                  </div>
-                  <br/>
+                </div>
+                <br />
                 <div>
-                  <br/>
-                                <span> Orbit </span>
-                            <select
-                                value={this.state.orbit}
-                                onChange={this.handleInputChange}
-                                name="orbit"
-                                placeholder="orbit"
-                            >
-                                <option value=""></option>
-                                <option value="Atira">Atira</option>
-                                <option value="Inner or Outer Main-belt">Inner or Outer Main-belt</option>
-                                <option value="Aten">Aten</option>
-                                <option value="Parabolic">TransNeptunian</option>
-                                <option value=" Jupiter Trojan"> Jupiter Trojan</option>
-                                <option value="Amor">Amor</option>
-                                <option value="Apollo">Apollo</option>
+                  <br />
+                  <span> Orbit </span>
+                  <select
+                    value={this.state.orbit}
+                    onChange={this.handleInputChange}
+                    name="orbit"
+                    placeholder="orbit"
+                  >
+                    <option value="" />
+                    <option value="Inner or Outer Main-belt">
+                      Inner or Outer Main-belt
+                    </option>
+                    <option value="Aten">Aten</option>
+                    <option value="Parabolic">TransNeptunian</option>
+                    <option value=" Jupiter Trojan"> Jupiter Trojan</option>
+                    <option value="Amor">Amor</option>
+                    <option value="Apollo">Apollo</option>
                   </select>
-                  </div>
-                  <br/>
-                  
-                    <div>
-                  <br/>
-                                <span> Spectrograph filter </span>
-                            <select
-                                value={this.state.filter}
-                                onChange={this.handleInputChange}
-                                name="filter"
-                                placeholder="spectrograph filter"
-                            >
-                                <option value=""></option>
-                                <option value='BG38	2 mm' >BG38	2 mm</option>
-                                <option value='WG360	1 mm'>WG360	1 mm</option>
-                                <option value='Moon	1.25"' >Moon 1.25"</option>
-                                <option value='82A Light Blue' >82A Light Blue"</option>
-                                
-                                   
+                </div>
+                <br />
+
+                <div>
+                  <br />
+                  <span> Spectrograph filter </span>
+                  <select
+                    value={this.state.filter}
+                    onChange={this.handleInputChange}
+                    name="filter"
+                    placeholder="spectrograph filter"
+                  >
+                    <option value="" />
+                    <option value="BG38	2 mm" hidden={this.state.coronado}>
+                      BG38 2 mm
+                    </option>
+                    <option value="WG360	1 mm" hidden={this.state.coronado}>
+                      WG360 1 mm
+                    </option>
+                    <option value="Moon	1.25&quot;" hidden={this.state.coronado}>
+                      Moon 1.25"
+                    </option>
+                    <option value="82A Light Blue" hidden={this.state.coronado}>
+                      82A Light Blue
+                    </option>
+                    <option
+                      value="Hydrogen Alpha"
+                      hidden={
+                        this.state.celestron ||
+                        this.state.meade ||
+                        this.state.bc
+                      }
+                    >
+                      Hydrogen-alpha
+                    </option>
+                    <option
+                      value="Solar 90mm"
+                      hidden={
+                        this.state.celestron ||
+                        this.state.meade ||
+                        this.state.bc
+                      }
+                    >
+                      {" "}
+                      Solar 90mm
+                    </option>
                   </select>
-                  </div>
-                  <br/>
-
-
-
-
-
-
-
+                </div>
+                <br />
 
                 <TextArea
-                value={this.state.comment}
+                  value={this.state.comment}
                   name="comment"
                   onChange={this.handleInputChange}
                   placeholder="Comment"
@@ -198,7 +266,6 @@ class Entry extends Component {
                   Submit Entry
                 </FormBtn>
               </form>
-            
             </Jumbotron>
           </Col>
         </Row>
@@ -208,4 +275,3 @@ class Entry extends Component {
 }
 
 export default Entry;
-
